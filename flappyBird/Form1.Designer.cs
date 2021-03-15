9d27027c2cc6f8aa6ae883328cab37e446a67b7e
thygo de jong <thygodejong@gmail.com>
2021-03-15 11:34:01 +0100

    merge

--- flappyBird/Form1.Designer.cs
+++ flappyBird/Form1.Designer.cs
@@ -30,17 +30,31 @@ namespace flappyBird
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(382, 388);
+            this.button1.Font = new System.Drawing.Font("Impact", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(392, 406);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(239, 97);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "start game";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Impact", 25.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Orange;
+            this.label1.Location = new System.Drawing.Point(339, 262);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(347, 85);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "FlappyBird";
             // 
             // Form1
             // 
@@ -49,15 +63,18 @@ namespace flappyBird
             this.BackgroundImage = global::flappyBird.Properties.Resources.flappy_bird_background_4;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(986, 804);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
